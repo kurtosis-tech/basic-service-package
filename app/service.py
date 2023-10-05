44cf38274254dc6c31af2faf808f22c074484a80
galenmarchetti <galenmarchetti@gmail.com>
2023-10-05 13:26:39 -0600

    service

--- app/service.py
+++ app/service.py
@@ -1,4 +1,12 @@
 # An extremely basic 'service' built using Python and Streamlit
 import streamlit as st
+import json
+
+CONFIG_PATH = "service-config.json"
 
 st.write("Hello, I'm an extremely basic service.")
+
+with open(CONFIG_PATH) as config_fp:
+    config_file = json.load(config_fp)
+
+st.write("My configuration file is: " + str(config_file))
